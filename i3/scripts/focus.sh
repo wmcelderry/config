7e9434ba2ec7d2fb77053a9afe7c9eb31c126d0b
Will McElderry <will@mcelderry.com>
2018-05-11 16:39:37 +0100

    Remove useless scripts.

--- i3/scripts/focus.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tag=$1
-
-i3-msg '[con_mark="'${tag}'"] focus'
-i3-msg mode "default"
-
-
--- i3/scripts/move.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-target=$1
-
-i3-msg  'move scratchpad; [con_mark="'${target}'"] focus ; scratchpad show ;  floating disable ; mode "default"'
