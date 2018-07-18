c03caab6f9590cc66d698c13c02426509d0bc215
Will McElderry <wm-git@switchd.net>
2018-07-18 22:49:47 +0100

    Script to plot the status of the battery from the log.

---
+++ scripts/plotBatt.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/gnuplot
+
+set timefmt "%d-%m-%Y %H:%M:%S"
+
+set autoscale y
+set autoscale x
+
+set format x "%d-%m %H:%M"
+
+set xdata time
+set style line 1 lt 1 lw 1
+plot "battery.log" using 1:4 w l ls 1
+
+
+pause -1
