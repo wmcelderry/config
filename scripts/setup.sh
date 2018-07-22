60ccd6fa1e85a12ae011c7713cb074eba387826c
Will McElderry <wm-git@switchd.net>
2018-07-22 15:27:37 +0100

    install ESP32 Arduino environment

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 function listSW()
 {
 #admin tools
@@ -150,10 +151,26 @@ EOF
 
 }
 
+function installESP32Arduino()
+{
+	wget https://bootstrap.pypa.io/get-pip.py
+	sudo python get-pip.py
+	sudo pip install pyserial
+	mkdir -p ~/Arduino/hardware/espressif
+	cd ~/Arduino/hardware/espressif
+	git clone https://github.com/espressif/arduino-esp32.git esp32
+	cd esp32
+	git submodule update --init --recursive
+	cd tools
+	python2 get.py
+}
+
 function nonIntegratedSW()
 {
 #arduino
 	installArduino
+#ESP32-arduino:
+	installESP32Arduino
 }
 
 
